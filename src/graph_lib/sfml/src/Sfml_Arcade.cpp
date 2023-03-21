f331a10b7f021f20be0c9d8561eca2218b2f9f1f
zvillon <zhg.villon@gmail.com>
2023-03-21 15:41:03 +0100

    feat(Files)Using src file with fpic instead of .o

--- src/graph_lib/sfml/src/Sfml_Arcade.cpp
+++ src/graph_lib/sfml/src/Sfml_Arcade.cpp
@@ -10,23 +10,23 @@
 
 Sfml_Arcade::Sfml_Arcade()
 {
-    // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // while (window.isOpen())
-    // {
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    //     window.clear();
-    //     window.draw(shape);
-    //     window.display();
-    // }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
 
 Sfml_Arcade::~Sfml_Arcade()
@@ -49,8 +49,7 @@ void Sfml_Arcade::draw(std::shared_ptr<arcade::IObject> object)
 
 arcade::Input Sfml_Arcade::event()
 {
-    return arcade::Input::ACTION1
-    ;
+    return arcade::Input::ACTION1;
 }
 
 extern "C" {
