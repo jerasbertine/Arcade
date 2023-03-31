4287e8a6a1a824176b6b2c161bba3a1dafd6eaa7
Jeras Bertine <jeras.bertine@epitech.eu>
2023-03-31 13:55:47 +0200

    feat(pacman) map parsing, pacman parsing and display

--- src/game_lib/pac-man/src/PacMan.cpp
+++ src/game_lib/pac-man/src/PacMan.cpp
@@ -9,17 +9,177 @@
 
 PacMan::PacMan()
 {
+    this->initMap(MAPPATH);
+}
+
+void PacMan::setPosition(short i_x, short i_y)
+{
+	this->_pacmanPos = {i_x, i_y};
 }
 
 PacMan::~PacMan()
 {
 }
 
+void PacMan::inputEvent(arcade::Input input)
+{
+    switch (input) {
+        case arcade::Input::UP:
+            std::cout << "UP" << std::endl;
+            break;
+        case arcade::Input::DOWN:
+            std::cout << "DOWN" << std::endl;
+            break;
+        case arcade::Input::LEFT:
+            std::cout << "LEFT" << std::endl;
+            break;
+        case arcade::Input::RIGHT:
+            std::cout << "RIGHT" << std::endl;
+            break;
+        default:
+            break;
+    }
+}
+
+void PacMan::setScore()
+{
+    auto score = createText();
+    score->setText("Score: ");
+    score->setColorText(arcade::Color::WHITE);
+    score->setPosition({700, 50});
+    this->_object.push_back(score);
+}
+
+void PacMan::setLevel()
+{
+    auto level = createText();
+    level->setText("Level: ");
+    level->setColorText(arcade::Color::WHITE);
+    level->setPosition({700, 150});
+    this->_object.push_back(level);
+}
+
+void PacMan::setEnergizer()
+{
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 21; j++) {
+            if (this->_map[i][j] == 'o') {
+                auto energizer = createTile();
+                energizer->setColor(arcade::Color::RED);
+                setPosition(j * 30, i * 30);
+                energizer->setPosition({j * 30, i * 30});
+                this->_object.push_back(energizer);
+            }
+        }
+    }
+}
+
+void PacMan::setFood()
+{
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 21; j++) {
+            if (this->_map[i][j] == '.') {
+                auto food = createTile();
+                food->setColor(arcade::Color::YELLOW);
+                setPosition(j * 30, i * 30);
+                food->setPosition({j * 30, i * 30});
+                this->_object.push_back(food);
+            }
+        }
+    }
+}
+
+void PacMan::setGhost()
+{
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 21; j++) {
+            if (this->_map[i][j] == '0') {
+                auto ghost1 = createTile();
+                this->_ghostPos[0].x = j * 30;
+                this->_ghostPos[0].x = i * 30;
+                ghost1->setColor(arcade::Color::RED);
+                ghost1->setPosition({j * 30, i * 30});
+                this->_object.push_back(ghost1);
+            }
+            if (this->_map[i][j] == '1') {
+                auto ghost2 = createTile();
+                this->_ghostPos[1].x = j * 30;
+                this->_ghostPos[1].x = i * 30;
+                ghost2->setColor(arcade::Color::YELLOW);
+                ghost2->setPosition({j * 30, i * 30});
+                this->_object.push_back(ghost2);
+            }
+            if (this->_map[i][j] == '2') {
+                auto ghost3 = createTile();
+                this->_ghostPos[2].x = j * 30;
+                this->_ghostPos[2].x = i * 30;
+                ghost3->setColor(arcade::Color::BLUE);
+                ghost3->setPosition({j * 30, i * 30});
+                this->_object.push_back(ghost3);
+            }
+            if (this->_map[i][j] == '3') {
+                auto ghost4 = createTile();
+                this->_ghostPos[3].x = j * 30;
+                this->_ghostPos[3].x = i * 30;
+                ghost4->setColor(arcade::Color::GREEN);
+                ghost4->setPosition({j * 30, i * 30});
+                this->_object.push_back(ghost4);
+            }
+        }
+    }
+}
+
+void PacMan::setWall()
+{
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 21; j++) {
+            if (this->_map[i][j] == '#') {
+                auto wall = createTile();
+                wall->setColor(arcade::Color::BLUE);
+                wall->setPosition({j * 30, i * 30});
+                this->_object.push_back(wall);
+            }
+        }
+    }
+}
+
+void PacMan::setPacman()
+{
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 21; j++) {
+            if (this->_map[i][j] == 'P') {
+                auto pacman = createTile();
+                pacman->setColor(arcade::Color::YELLOW);
+                setPosition(j * 30, i * 30);
+                pacman->setPosition({j * 30, i * 30});
+                this->_object.push_back(pacman);
+            }
+        }
+    }
+}
+
+void PacMan::setText()
+{
+    setScore();
+    setLevel();
+}
+
+void PacMan::setMap()
+{
+    setWall();
+    setPacman();
+    setGhost();
+    setFood();
+    setEnergizer();
+}
+
 std::vector<std::shared_ptr<arcade::IObject>> PacMan::loop(arcade::Input input)
 {
-    // TODO loop
-    std::vector<std::shared_ptr<arcade::IObject>> vector;
-    return vector;
+    inputEvent(input);
+    this->_object.clear();
+    setMap();
+    setText();
+    return this->_object;
 }
 
 void PacMan::restart()
@@ -27,25 +187,32 @@ void PacMan::restart()
     return;
 }
 
+void PacMan::initMap(std::string path)
+{
+    std::string line;
+    std::ifstream file(path);
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            this->_map.push_back(line);
+        }
+        file.close();
+    }
+}
+
 std::shared_ptr<arcade::ITile> PacMan::createTile()
 {
-    // TODO createTile
-    std::shared_ptr<arcade::ITile> tile;
-    return tile;
+    return std::make_shared<arcade::ATile>();
 }
 
 std::shared_ptr<arcade::ISound> PacMan::createSound()
 {
-    // TODO createSound
-    std::shared_ptr<arcade::ISound> sound;
-    return sound;
+    return std::make_shared<arcade::ASound>();
 }
 
 std::shared_ptr<arcade::IText> PacMan::createText()
 {
-    //TODO createText
-    std::shared_ptr<arcade::IText> text;
-    return text;
+    return std::make_shared<arcade::AText>();
 }
 
 extern "C" {
