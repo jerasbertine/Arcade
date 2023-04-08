5d3c71e1b8d4eac97e8f0f8cbb862102c57ab588
zvillon <zhg.villon@gmail.com>
2023-04-08 21:42:10 +0200

    feat(ncurses) Ncurses lib

--- src/graph_lib/ncurses/include/Ncurses_Arcade.hpp
+++ src/graph_lib/ncurses/include/Ncurses_Arcade.hpp
@@ -9,7 +9,14 @@
     #define NCURSES_ARCADE_HPP_
     #include <ncurses.h>
     #include <sys/stat.h>
+    #include "../../../../include/Error.hpp"
     #include "../../../../include/Arcade-Architecture/IGraphics.hpp"
+
+    #define KEY_ESC 27
+    #define KEY_B 98
+    #define KEY_G 103
+    #define KEY_H 104
+    #define KEY_N 110
 
 class NcursesArcade : public arcade::IGraphics {
     public:
@@ -19,6 +26,9 @@ class NcursesArcade : public arcade::IGraphics {
         void clear();
         void draw(std::shared_ptr<arcade::IObject> object);
         arcade::Input event();
+        void handleTile(std::shared_ptr<arcade::ITile> tile);
+        void handleSound(std::shared_ptr<arcade::ISound> sound);
+        void handleText(std::shared_ptr<arcade::IText> text);
 
     protected:
     private:
--- src/graph_lib/ncurses/src/Ncurses_Arcade.cpp
+++ src/graph_lib/ncurses/src/Ncurses_Arcade.cpp
@@ -10,29 +10,101 @@
 NcursesArcade::NcursesArcade()
 {
     initscr();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
 }
 
 NcursesArcade::~NcursesArcade()
 {
+    endwin();
 }
 
 void NcursesArcade::display()
 {
-    std::cout << "Display ncurses" << std::endl;
+    
 }
 
 void NcursesArcade::clear()
 {
-    clear();
+    refresh();
+}
+
+void NcursesArcade::handleTile(std::shared_ptr<arcade::ITile> tile)
+{
+
+}
+
+void NcursesArcade::handleSound(std::shared_ptr<arcade::ISound> sound)
+{
+
+}
+
+void NcursesArcade::handleText(std::shared_ptr<arcade::IText> text)
+{
+    
 }
 
 void NcursesArcade::draw(std::shared_ptr<arcade::IObject> object)
 {
+    std::shared_ptr<arcade::ITile> tile = std::dynamic_pointer_cast<arcade::ITile>(object);
+    if (tile != nullptr) {
+        handleTile(tile);
+        return;
+    }
+    std::shared_ptr<arcade::ISound> sound = std::dynamic_pointer_cast<arcade::ISound>(object);
+    if (sound != nullptr) {
+        handleSound(sound);
+        return;
+    }
+    std::shared_ptr<arcade::IText> text = std::dynamic_pointer_cast<arcade::IText>(object);
+    if (text != nullptr) {
+        handleText(text);
+        return;
+    }
 }
 
 arcade::Input NcursesArcade::event()
 {
-    return arcade::Input::PREVIOUSGRAPH;
+    int key = getch();
+    arcade::Input event = arcade::Input::UNDEFINED;
+
+    switch (key) {
+        case KEY_ESC:
+            event = arcade::Input::EXIT;
+            endwin();
+            break;
+        case KEY_UP:
+            event = arcade::Input::UP;
+            break;
+        case KEY_DOWN:
+            event = arcade::Input::DOWN;
+            break;
+        case KEY_RIGHT:
+            event = arcade::Input::RIGHT;
+            break;
+        case KEY_LEFT:
+            event = arcade::Input::LEFT;
+            break;
+        case KEY_B:
+            event = arcade::Input::PREVIOUSGAME;
+            break;
+        case KEY_N:
+            event = arcade::Input::NEXTGAME;
+            break;
+        case KEY_G:
+            event = arcade::Input::PREVIOUSGRAPH;
+            endwin();
+            break;
+        case KEY_H:
+            event = arcade::Input::NEXTGRAPH;
+            endwin();
+            break;
+        default:
+            break;
+    }
+
+    return event;
 }
 
 extern "C" {
